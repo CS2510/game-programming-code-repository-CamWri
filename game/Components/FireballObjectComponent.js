feafd928d9b68778f4984ba20e997dabe13c5c01
CamWri <camdenawright2022@gmail.com>
2026-02-22 23:40:09 -0600

    End of things for fireball

--- game/Components/FireballObjectComponent.js
+++ game/Components/FireballObjectComponent.js
@@ -4,7 +4,6 @@ class FireballObjectComponent extends Component{
     }
 
     onDestroy(){
-        GameObject.find("Turn Manager Game Object").getComponent(TurnManagerComponent).currentCharacter.getComponent(CharacterComponent).activeAbility = null
         instantiate(new FireballAOEGameObject(), this.transform.position.clone())
     }
 }
